4c82e0cd28fdd74d3aca58b9652aa0c21660152c
sihagwak <sasha.gwak@gmail.com>
2022-07-03 23:13:30 +0900

    study: node test

---
+++ main.js
@@ -0,0 +1,4 @@
+let m = require('./math_tools.js');
+// 모듈을 로드해서 객체 1개를 리턴, 리턴하는 것을 쓸 수 있도록 변수로 저장 
+
+console.log(m.add(1,2));
---
+++ math_tools.js
@@ -0,0 +1,4 @@
+//module -> js 파일 하나 의미
+function add(a, b) {
+    return a + b;
+}
