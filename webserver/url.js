3dde53fc9c541bd2edec6ee58c78b39a09585233
sihagwak <sasha.gwak@gmail.com>
2022-07-17 02:06:53 +0900

    study: url file

---
+++ webserver/url.js
@@ -0,0 +1,7 @@
+// URL 쪼개기 
+let url = new URL('https://example.com/business/mart/item?category=14&id=2965')
+
+console.log(url.protocol)
+console.log(url.host)
+console.log(url.pathname)
+console.log(url.search)
