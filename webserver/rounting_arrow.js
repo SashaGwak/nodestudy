efeb24a9d559b8164f0e008d3bed5821fcee17ee
sihagwak <sasha.gwak@gmail.com>
2022-07-19 23:48:55 +0900

    study: routing file

--- webserver/rounting_arrow.js
+++ webserver/rounting_arrow.js
@@ -1,18 +1,21 @@
 // http rounting.js 파일 arrow 버전
 const { request } = require("http");
 
-let users = ['Tom', 'Andy', 'Jessica', 'Paul'];
+const users = ['Tom', 'Andy', 'Jessica', 'Paul'];
+// 변수는 가능하다면 최대한 상수로 바꾸기
 
-let server = http.createServer((request, response) => {
+const server = http.createServer((request, response) => {
     if (request.url === '/') {
         response.end('<h1>Welcome!</h1>');
     } else if (request.url === '/users') {
         response.end('<h1>' + users + '</h1>');
     } else if (request.url.split('/')[1] === 'users') {
-        let userIdx = request.url.split('/')[2];
-        let userName = users[userIdx -1];
+        const userIdx = request.url.split('/')[2];
+        const userName = users[userIdx -1];
 
         response.end('<h1>' + userName + '</h1>'); 
+        // 변수가 섞인 문자열은 + 기보호다는 템플릿 문자열 사용하기
+        // (`<h1>${userName}</h1>`)
     } else {
         response.end('<h1>Page Not Available</h1>')
     }
