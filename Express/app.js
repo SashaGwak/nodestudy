11ca9835eff6c2b7610783ee12797f2a3647fb2e
sihagwak <sasha.gwak@gmail.com>
2022-07-26 23:35:27 +0900

    study: json 정보 처리하기

--- Express/app.js
+++ Express/app.js
@@ -6,7 +6,14 @@ const members = require('./members');
 
 // json 형태로 저장되어 있는 member 정보 모두 출력하기
 app.get('/api/members', (req, res) => {
-    res.send(members);
+    const {team} = req.query; 
+    // const team = req.query.team 과 동일한 내용
+    if (team) {
+        const teamMembers = members.filter((m) => m.team === team);
+        res.send(teamMembers);
+    } else {
+        res.send(members);
+    }
 })
 
 // id 별로 직원 정보 출력하기 위한 코드
