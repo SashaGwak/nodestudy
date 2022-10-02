69bae9506dd3c41f81a64d0d9d23bb8a660a178b
sihagwak <sasha.gwak@gmail.com>
2022-10-02 23:24:46 +0900

    feat: create add product process

--- projectShop/routes/shop.js
+++ projectShop/routes/shop.js
@@ -6,10 +6,12 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
+// 상품 페이지
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+// 장바구니 페이지
 router.get('/cart',shopController.getCart);
 
 router.get('/checkout', shopController.getCheckout);
