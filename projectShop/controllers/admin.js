d7abb8143635f5b7af15e9b34d61bdb0e9cce615
sihagwak <sasha.gwak@gmail.com>
2022-10-03 16:56:43 +0900

    feat: product edit 추가, 변경 페이지 렌더링

--- projectShop/controllers/admin.js
+++ projectShop/controllers/admin.js
@@ -1,13 +1,11 @@
 const Product = require('../models/product');
 
-// 상품 검색 페이지 
+// 상품 추가 페이지 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false, 
   });
 };
 
@@ -20,6 +18,26 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+// 상품 편집 페이지 
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode){
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId; 
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode, 
+      product: product
+    });
+  })
 };
 
 
--- projectShop/routes/admin.js
+++ projectShop/routes/admin.js
@@ -7,11 +7,15 @@ const router = express.Router();
 
 // 상품 추가 페이지
 router.get('/add-product', adminController.getAddProduct);
+
 // 상품 관리 페이지
 router.get('/products', adminController.getProducts);
 
 // 상품 추가 기능
 router.post('/add-product', adminController.postAddProduct);
+
+// 상품 정보 변경 기능 
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 module.exports = router;
 
