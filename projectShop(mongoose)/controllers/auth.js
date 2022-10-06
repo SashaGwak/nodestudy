6d5b38530e1089c1a1a39ecf939b275d30a07453
sihagwak <sasha.gwak@gmail.com>
2022-10-06 23:25:00 +0900

    feat: 회원가입 기능 추가

--- projectShop(mongoose)/controllers/auth.js
+++ projectShop(mongoose)/controllers/auth.js
@@ -10,6 +10,14 @@ exports.getLogin = (req, res, next) => {
   });
 }; 
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('633dabf05d188594892a37cd')
   .then(user => {
@@ -25,6 +33,34 @@ exports.postLogin = (req, res, next) => {
   // 헤더의 이름을 Set-Cookie로 지정
   // 헤더 값의 가장 간단한 형식은 키-값쌍
   // Secure을 추가할 경우 HTTPS를 통해 페이지가 제공될 경우에만 설정됨, HttpOnly로 http 전용으로만 설정할 수도 있음
+};
+
+// 회원가입 기능
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email; 
+  const password = req.body.password; 
+  const confirmPassword = req.body.confirmPassword;
+  User
+    .findOne({email: email})
+    .then(userDoc => {
+      // 만약 이메일이 이미 존재하면
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      // 아니라면 
+      const user = new User({
+        email: email, 
+        password: password, 
+        cart: { items: [] } 
+      }); 
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
--- projectShop(mongoose)/routes/auth.js
+++ projectShop(mongoose)/routes/auth.js
@@ -7,8 +7,14 @@ const router = express.Router();
 /* 로그인 페이지 */
 router.get('/login', authController.getLogin); 
 
+/* 회원가입 페이지 */
+router.get('/signup', authController.getSignup);
+
 /* 로그인 기능 */
 router.post('/login', authController.postLogin); 
+
+/* 회원가입 기능 */
+router.post('/signup', authController.postSignup);
 
 /* 로그아웃 기능 */
 router.post('/logout', authController.postLogout); 
